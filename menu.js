46c9a3e705fa06236427134082241d30c0ed4303
Evelyn de Paula <evelyn.depiala@gmail.com>
2021-07-30 18:01:25 -0300

    first commit

---
+++ menu.js
@@ -0,0 +1,6 @@
+const hamburguer = document.querySelector('.hamburguer');
+const navLinks = document.querySelector('.links__menu');
+
+hamburguer.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+});
